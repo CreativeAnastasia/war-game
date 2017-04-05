bf03f9c44a4787bed60f05c9c88d6e8a34154dd3
Anastasia Netz Netz <creativeanastasia@gmail.com>
2017-04-04 19:07:26 -0700

    commit_recent

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 // <------- variables ------->
-var player1, player2;
+var inPlay1, inPlay2;
+var winner, loser;
 var deck, deck1, deck2;
+var display1, display2;
+var didWar;
+
+
+    $('#battle').hide();
+    $('#war').hide();
+    $('#win1').hide();
+    $('#win2').hide();
 
 
 
 // <------- cached dom elements ------->
 var $card1 = $('#card1');
 var $card2 = $('#card2');
+var $board = $('#board');
 
 
 
 // <------- event listeners ------->
 $('#newgame').on('click', startGame)
 $('#battle').on('click', dealBattle)
+$('#restart').on('click', startGame)
+$('#rules').on('click', function () {
+                var w = window.open("", "popupWindow", "width=600, height=400, scrollbars=yes");
+                var $w = $(w.document.body);
+                $w.html("<textarea>Hello I am rules</textarea>");
+            });
+
 
 
 
@@ -53,64 +70,91 @@ function splitDeck(array){
 function startGame(){
   // initialize variables
   deck1 = deck2 = [];
+  loser = 0;
+  display1 = display2 = null;
   splitDeck(shuffleDeck(buildDeck()));
+  dealBattle();
+  $('#war').hide();
+  $('#win1').hide();
+  $('#win2').hide();
+
 }
 
 
 //dealBattle -> render function possibly?
 function dealBattle() {
-  if (deck1.length > 0){
-    var card = deck1.shift();
-    player1 = card;
+  $('#battle').show();
+  didWar = false;
+  inPlay1 = [];
+  inPlay2 = [];
+  if (deck1.length){
+    inPlay1.push(deck1.shift());
+  } else{
+    loser = 1;
+  }
+  if (deck2.length){
+    inPlay2.push(deck2.shift());
+  } else{
+    loser = 2;
+  }
+  winner = getWinner();
+  if (winner === 1) {
+    $('#win2').hide();
+    $('#win1').show();
+  } else if (winner ===2){
+    $('#win1').hide();
+    $('#win2').show();
+  }
+  while (!winner && !loser) {
+    didWar = true;
+    dealWar();
+    winner = getWinner();
+  }
+  var victor = winner === 1 ? deck1 : deck2;
+  victor.push(...inPlay1);
+  victor.push(...inPlay2);
+  display1 = inPlay1[0].css;
+  display2 = inPlay2[0].css;
+  render();
+}
 
+function dealWar() {
+  if (deck1.length < 4) {
+    loser = 1;
+  } else if (deck2.length < 4) {
+    loser = 2;
+  } else {
+    for (var i=0; i < 4; i++){
+      inPlay1.unshift(deck1.shift());
+      inPlay2.unshift(deck2.shift());
+    }
+  }
+}
 
+function render() {
+  if (display1) {
+    $('#newgame').hide();
+    $('#deck1').text(deck1.length);
+    $('#deck2').text(deck2.length);
     $card1.removeClass();
-    $card1.addClass('card ' + card.css);
-
-  } else{
-    return null;
-  }
-
-
-  if (deck2.length > 0){
-    var card = deck2.shift();
-    player2 = card;
-
-
+    $card1.addClass('card ' + display1);
     $card2.removeClass();
-    $card2.addClass('card ' + card.css);
-
-
-  } else{
-    return null;
-  }
-
-
-  if (player1.rank > player2.rank) {
-    deck1.push(player1);
-    deck1.push(player2);
-    console.log ("Player 1 wins");
-    console.log (deck1.length);
-    console.log (deck2.length);
-  }
-
-  if (player1.rank < player2.rank) {
-    deck2.push(player2);
-    deck2.push(player1);
-    console.log ("Player 2 wins");
-    console.log (deck1.length);
-    console.log (deck2.length);
-
-  }
-  if (player1.rank === player2.rank) {
-    gameWar();
+    $card2.addClass('card ' + display2);
+    // if loser display that message
+    // otherwise display battle loser message
+  } else {
+    $('#newgame').show();
   }
 }
 
-function gameWar(){
-  console.log ("In the war function");
-
+function getWinner() {
+  if (inPlay1[0].rank === inPlay2[0].rank) return 0;
+  return inPlay1[0].rank > inPlay2[0].rank ? 1 : 2;
 }
+
+
+
+
 
 
 
